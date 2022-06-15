0a9167a941aafcf2bd42af51817ec761787d2257
Bea <janeramerindo@gmail.com>
2022-06-15 19:29:56 -0300

    a

--- test.spec.js
+++ test.spec.js
@@ -3,13 +3,16 @@
  * teste integrado -> testa comportamento de integração(usecase chama o repositiro, integração de service(isto, é, um envio de email por exemplo, chamada a outra api..)
  * e2e -> teste de integração -> real, com banco e api externas -> request -> controller - usecase - service <- usecase <- controller <- response
  */
+const repositoryPokemon = { update (request) { return request } }
 
 describe('example', () => {
   it('should ', () => {
-      const cenario = "oi"
+      const createPokemon = {} // criando no banco...
+      const requestPokemon = {} // atualizacoes requisitadas
+      const expectedPokemon = {} // retorno esperado pos atualizacao
 
-      const resultado = "oi"
+      const resultado = repositoryPokemon.update(requestPokemon) // testa o comportamento
 
-      expect(cenario).toEqual(resultado)
+      expect(expectedPokemon).toEqual(resultado) // aqui vc faz as validacoes
   });
 });
